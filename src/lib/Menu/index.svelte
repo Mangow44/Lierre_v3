a1ec01b67ca27dd8178092f74054e24cbf410f38
Thomas Clouet <thomas.clouet1@etu.univ-nantes.fr>
2022-08-14 11:21:17 +0200

    :construction: Menu v1

--- src/lib/Menu/index.svelte
+++ src/lib/Menu/index.svelte
@@ -1,14 +1,36 @@
 <script>
+	import { onMount } from 'svelte';
+
 	export let displayMenu = false;
+
+	let screenWidth = 0;
+
+	onMount(() => {
+		const updateScreenWidth = () => {
+			screenWidth = window.innerWidth;
+		};
+		window.addEventListener('resize', updateScreenWidth);
+		updateScreenWidth();
+	});
 </script>
 
-<img
-	class="fixed z-10 h-[150%] w-full 
-        object-cover menu-transition
-        {displayMenu ? 'display-menu' : 'hide-menu'}"
-	src="/svg/menu.svg"
-	alt="menu svg"
-/>
+<div
+	id="menu"
+	class="fixed flex justify-center z-10 w-full h-full
+		menu-transition
+		{displayMenu ? 'display-menu' : 'hide-menu'}"
+>
+	<span
+		id="menu-rect"
+		class="absolute top-0 w-full"
+		style="background-color: #0D0D0D;height:calc(100% - {screenWidth / 2}px);"
+	/>
+	<span
+		id="menu-circle"
+		class="absolute mx-auto bottom-0 w-full rounded-full"
+		style="background-color: #0D0D0D;height:{screenWidth}px;"
+	/>
+</div>
 
 <style>
 	.menu-transition {
@@ -20,6 +42,6 @@
 	}
 
 	.display-menu {
-		transform: translateY(-10%);
+		transform: translateY(0%);
 	}
 </style>
