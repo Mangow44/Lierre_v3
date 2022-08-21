966d4e118bc8c21789b1bfe72153c2c26584fc1f
Thomas Clouet <thomas.clouet1@etu.univ-nantes.fr>
2022-08-21 15:29:27 +0200

    :art: Few improvements

--- src/lib/Data/pictures.js
+++ src/lib/Data/pictures.js
@@ -1,4 +1,8 @@
 export const pictures = [
+	{ src: '/pictures/momo_the_wind_mage.png', alt: 'momo-the-wind-mage', color: '#C8A880' },
+	{ src: '/pictures/loretta.png', alt: 'loretta', color: '#D5E2CD' },
+	{ src: '/pictures/gao_mochi.jpg', alt: 'gao-mochi', color: '#FEFFFE' },
 	{ src: '/pictures/diablotin.png', alt: 'diablotin', color: '#FEE1DD' },
-	{ src: '/pictures/momo_the_wind_mage.png', alt: 'momo-the-wind-mage', color: '#C8A880' }
+	{ src: '/pictures/le_tablier.jpeg', alt: 'le-tablier', color: '#765A45' },
+	{ src: '/pictures/lierre.jpeg', alt: 'lierre', color: '#FEFFFE' }
 ];
